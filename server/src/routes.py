6bd1d730c925a1a1fea44b2eb8d39b75e5f196a8
TemkaBanana <temka_banana@mail.ru>
2019-02-27 21:36:14 +0300

    add db group query

--- server/src/routes.py
+++ server/src/routes.py
@@ -5,4 +5,5 @@ def setup_routes(app):
     app.router.add_get('/object_classes', object_classes)
     app.router.add_get('/representation', representation)
     app.router.add_get('/group_representation', group_representation)
+    app.router.add_get('/main', main)
 
